aef77df7f22f917b63498d67045e15f457a9cdc2
alipoorseyed <poorseyedali40@gmail.com>
2024-07-12 00:10:27 +0330

    add the local storeg logic to project

--- main.js
+++ main.js
@@ -18,10 +18,128 @@ let done_item_text = document.querySelector(".done-title p");
 
 
 
+let todo_list_items_storeg = JSON.parse(localStorage.getItem("todo"));
 let todo_list_items = [];
 let todo_list_items_done = [];
 let prioritystring = "";
-// let counter = 0;
+
+
+
+
+
+for (let i = 0; i < todo_list_items_storeg.length; i++) {
+    // Create a temporary container to convert the string to a DOM Node
+    let tempDiv = document.createElement('div');
+    tempDiv.innerHTML = todo_list_items_storeg[i];
+    let node = tempDiv.firstChild;
+    // Append the node to the Todo_item_span
+    Todo_item_span.appendChild(node);
+    eventlisteners(node);
+    todo_list_items.push(node);
+}
+task_todo_text.innerHTML = `${todo_list_items.length} تسک را باید انجام دهید .`;
+if(todo_list_items.length!==0){
+    todo_pic.classList.add("To-do-pic-show");
+}
+
+
+
+function eventlisteners(node){
+
+    node.querySelector(".todoList_span_div_left").addEventListener("click" , editremove);
+    function editremove(){
+        console.log("messi");
+        node.querySelector(".todoList_span_div_left .editremove_span_div").classList.toggle("editremove_span_div_show");
+    }
+
+
+    node.querySelector(".todoList_span_div_left img[src='./images/tabler_trash-x.svg']").addEventListener("click" , trash_function);
+    function trash_function(){
+        for(let i=0 ; i<todo_list_items.length ; i++){
+            if(todo_list_items[i]===node){
+                todo_list_items[i].remove();
+                todo_list_items.splice(i,1);
+                break;
+            }
+        }
+        const todoListArray2 = Array.from(todo_list_items).map(div => div.outerHTML);
+        localStorage.setItem('todo', JSON.stringify(todoListArray2));
+
+        if(todo_list_items.length===0){
+            todo_pic.classList.remove("To-do-pic-show");
+        }
+
+        task_todo_text.innerHTML = `${todo_list_items.length} تسک را باید انجام دهید .`;
+    }
+
+
+
+
+    node.querySelector(".todoList_span_div_right_def_top input[type='checkbox']").addEventListener("click", todo_done);
+    function todo_done(){
+        let todo_done_span_div = node.cloneNode(true);
+        todo_done_span_div.className = "todo_done_span_div";
+        todo_done_span_div.querySelector(".todoList_span_div_right_def > p").remove();
+        todo_done_span_div.querySelector(".todoList_span_div_right_def_top div").remove();
+        done_item_span.appendChild(todo_done_span_div);
+        todo_list_items_done.push(todo_done_span_div);
+        done_item_text.innerHTML = `${todo_list_items_done.length} انجام شده است .`;
+
+        for(let i=0 ; i<todo_list_items.length ; i++){
+            if(todo_list_items[i]===node){
+                todo_list_items[i].remove();
+                todo_list_items.splice(i,1);
+                break;
+            }
+        }
+
+        const todoListArray3 = Array.from(todo_list_items).map(div => div.outerHTML);
+        localStorage.setItem('todo', JSON.stringify(todoListArray3));
+
+        
+        if(todo_list_items.length===0){
+            todo_pic.classList.remove("To-do-pic-show");
+        }
+
+        task_todo_text.innerHTML = `${todo_list_items.length} تسک را باید انجام دهید .`;
+
+    }
+
+
+
+
+
+    node.querySelector(".todoList_span_div_left img[src='./images/tabler_edit.svg']").addEventListener("click",edit_function);
+    function edit_function(){
+        let edit_div_span = document.getElementsByClassName("To-do-add-div")[0];
+        todo_submit.innerHTML = "ویرایش تسک";
+        input_title.value = node.querySelector(".todoList_span_div_right_def_top h4").innerHTML;
+        input_def.value = node.querySelector(".todoList_span_div_right_def > p").innerHTML;
+        if(document.getElementsByClassName("new_priority_tag")[0]===undefined){
+            todo_add_priority_localstoreg(node);   
+        }else{
+            edit_div_span.querySelector(".new_priority_tag p").innerHTML = node.querySelector(".priority_in_list > p").innerHTML;
+            edit_div_span.querySelector(".new_priority_tag").style.backgroundColor = node.querySelector(".todoList_span_div_right_line").style.backgroundColor;
+            edit_div_span.querySelector(".new_priority_tag").style.color = node.querySelector(".todoList_span_div_right_def_top .priority_in_list").style.color;
+        }           
+        node.insertAdjacentElement('afterend', edit_div_span);
+        edit_div_span.classList.toggle("To-do-add-div-show");
+        todo_submit.classList.add("down-bagcolor");
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -51,7 +169,6 @@ function todo_add_popup(){
 
 
 
-
 todo_priority_tag.addEventListener("click",todo_priority_popup);
 
 
@@ -59,10 +176,6 @@ function todo_priority_popup(){
     todo_priority_div.classList.toggle("To-do-priority-show");
     todo_priority_tag.classList.toggle("transform90");
 }
-
-
-
-
 
 
 todo_priority_high.addEventListener("click",todo_add_priority);
@@ -108,6 +221,44 @@ function todo_add_priority(event){
 
 
 
+function todo_add_priority_localstoreg(node){
+
+
+    if(input_def.value.trim() !== "" || input_title.value.trim() !== ""){
+        todo_submit.classList.add("down-bagcolor");
+    }
+    
+    prioritystring = node.querySelector(".priority_in_list > p").innerHTML;
+
+    let new_priority_tag = document.createElement("div");
+    let new_priority_tag_p = document.createElement("p");
+    let new_priority_tag_img = document.createElement("img");
+
+    new_priority_tag_p.innerText = prioritystring;
+    new_priority_tag_img.src = "./images/close-circle.svg";
+    new_priority_tag.appendChild(new_priority_tag_img);
+    new_priority_tag.appendChild(new_priority_tag_p);
+
+    new_priority_tag.style.backgroundColor = node.querySelector(".todoList_span_div_right_line").style.backgroundColor;
+    new_priority_tag.style.color = node.querySelector(".todoList_span_div_right_def_top .priority_in_list").style.color;
+    
+    new_priority_tag.setAttribute("class","new_priority_tag");
+    todo_priority_tag.parentNode.appendChild(new_priority_tag);
+
+    new_priority_tag_img.addEventListener("click",function(){
+        new_priority_tag.remove();
+        todo_priority_tag.classList.remove("To-do-tag-show");
+        todo_submit.classList.remove("down-bagcolor");
+    });
+
+    todo_priority_div.classList.remove("To-do-priority-show");
+    todo_priority_tag.classList.add("To-do-tag-show");
+    todo_priority_tag.classList.remove("transform90");
+
+}
+
+
+
 
 
 input_def.addEventListener("input", checkfuncion);
@@ -125,7 +276,6 @@ function checkfuncion(){
 
 
 
-
 todo_submit.addEventListener("click",todo_add_submit);
 
 function todo_add_submit(){
@@ -133,6 +283,18 @@ function todo_add_submit(){
         alert("شما باید نام تسک و توضیحات را وارد کنید و اولویت خود را انتخاب کنید .");
         return;
     }
+
+    if(todo_submit.innerHTML === "ویرایش تسک"){
+        for(let i=0 ; i<todo_list_items.length ; i++){
+            if(todo_list_items[i]===todo_add_div.previousSibling){
+                todo_list_items[i].remove();
+                todo_list_items.splice(i,1);
+                break;
+            }
+        }
+    }
+
+    
 
     let todoList_span_div = document.createElement("div");
     let todoList_span_div_right = document.createElement("div");
@@ -195,10 +357,10 @@ function todo_add_submit(){
 
 
 
-    todoList_span_div_left_image.addEventListener("click" , editremove);
-    function editremove(){
-        editremove_span_div.classList.toggle("editremove_span_div_show");
-    }
+    // todoList_span_div_left_image.addEventListener("click" , editremove);
+    // function editremove(){
+    //     editremove_span_div.classList.toggle("editremove_span_div_show");
+    // }
 
 
 
@@ -206,24 +368,22 @@ function todo_add_submit(){
 
 
 
-    trash_image.addEventListener("click" , trash_function);
-    function trash_function(){
-        for(let i=0 ; i<todo_list_items.length ; i++){
-            if(todo_list_items[i]===todoList_span_div){
-                todo_list_items[i].remove();
-                todo_list_items.splice(i,1);
-                break;
-            }
-        }
+    // trash_image.addEventListener("click" , trash_function);
+    // function trash_function(){
+    //     for(let i=0 ; i<todo_list_items.length ; i++){
+    //         if(todo_list_items[i]===todoList_span_div){
+    //             todo_list_items[i].remove();
+    //             todo_list_items.splice(i,1);
+    //             break;
+    //         }
+    //     }
 
-        if(todo_list_items.length===0){
-            todo_pic.classList.remove("To-do-pic-show");
-        }
+    //     if(todo_list_items.length===0){
+    //         todo_pic.classList.remove("To-do-pic-show");
+    //     }
 
-        task_todo_text.innerHTML = `${todo_list_items.length} تسک را باید انجام دهید .`;
-    }
-
-
+    //     task_todo_text.innerHTML = `${todo_list_items.length} تسک را باید انجام دهید .`;
+    // }
 
 
 
@@ -232,34 +392,34 @@ function todo_add_submit(){
 
 
 
-    todoList_span_div_right_def_top_check.addEventListener("click", todo_done);
-    function todo_done(){
-        let todo_done_span_div = todoList_span_div.cloneNode(true);
-        todo_done_span_div.className = "todo_done_span_div";
-        todo_done_span_div.querySelector(".todoList_span_div_right_def > p").remove();
-        todo_done_span_div.querySelector(".todoList_span_div_right_def_top div").remove();
-        done_item_span.appendChild(todo_done_span_div);
-        todo_list_items_done.push(todo_done_span_div);
-        done_item_text.innerHTML = `${todo_list_items_done.length} انجام شده است .`;
 
-        for(let i=0 ; i<todo_list_items.length ; i++){
-            if(todo_list_items[i]===todoList_span_div){
-                todo_list_items[i].remove();
-                todo_list_items.splice(i,1);
-                break;
-            }
-        }
+
+    // todoList_span_div_right_def_top_check.addEventListener("click", todo_done);
+    // function todo_done(){
+    //     let todo_done_span_div = todoList_span_div.cloneNode(true);
+    //     todo_done_span_div.className = "todo_done_span_div";
+    //     todo_done_span_div.querySelector(".todoList_span_div_right_def > p").remove();
+    //     todo_done_span_div.querySelector(".todoList_span_div_right_def_top div").remove();
+    //     done_item_span.appendChild(todo_done_span_div);
+    //     todo_list_items_done.push(todo_done_span_div);
+    //     done_item_text.innerHTML = `${todo_list_items_done.length} انجام شده است .`;
+
+    //     for(let i=0 ; i<todo_list_items.length ; i++){
+    //         if(todo_list_items[i]===todoList_span_div){
+    //             todo_list_items[i].remove();
+    //             todo_list_items.splice(i,1);
+    //             break;
+    //         }
+    //     }
 
         
-        if(todo_list_items.length===0){
-            todo_pic.classList.remove("To-do-pic-show");
-        }
+    //     if(todo_list_items.length===0){
+    //         todo_pic.classList.remove("To-do-pic-show");
+    //     }
 
-        task_todo_text.innerHTML = `${todo_list_items.length} تسک را باید انجام دهید .`;
+    //     task_todo_text.innerHTML = `${todo_list_items.length} تسک را باید انجام دهید .`;
 
-    }
-
-
+    // }
 
 
 
@@ -274,7 +434,9 @@ function todo_add_submit(){
 
 
 
-    // todoList_span_div.setAttribute("id" , `${counter}`);
+
+
+    // todoList_span_div.setAttribute("id" , ${counter});
     // counter++;
 
 
@@ -301,10 +463,16 @@ function todo_add_submit(){
         todo_list_items.splice(index,0,todoList_span_div);
      }
 
+
     for (let i = 0; i < todo_list_items.length; i++) {
         Todo_item_span.appendChild(todo_list_items[i]);
     }
 
+
+    // Save the array as a JSON string to local storage
+
+    const todoListArray2 = Array.from(todo_list_items).map(div => div.outerHTML);
+    localStorage.setItem('todo', JSON.stringify(todoListArray2));
 
 
     
@@ -318,6 +486,10 @@ function todo_add_submit(){
     task_todo_text.innerHTML = `${todo_list_items.length} تسک را باید انجام دهید .`;
     todo_add_div.classList.remove("To-do-add-div-show");
     todo_add_button.insertAdjacentElement('afterend' , todo_add_div);
+    todo_submit.classList.remove("down-bagcolor");
+
+
+
 
 
 
@@ -325,27 +497,24 @@ function todo_add_submit(){
     
     
 
-    edit_image.addEventListener("click",edit_function);
-    function edit_function(){
-        let edit_div_span = document.getElementsByClassName("To-do-add-div")[0];
-        todo_submit.innerHTML = "ویرایش تسک";
-        input_title.value = todoList_span_div_right_def_top_h4.innerHTML;
-        input_def.value = todoList_span_div_right_def_bottom.innerHTML;
-        edit_div_span.querySelector(".new_priority_tag p").innerHTML = priority_value.innerHTML;
-        edit_div_span.querySelector(".new_priority_tag").style.backgroundColor = todoList_span_div_right_line.style.backgroundColor;
-        edit_div_span.querySelector(".new_priority_tag").style.color = todoList_span_div_right_def_top_priority.style.color;      
-        todoList_span_div.insertAdjacentElement('afterend', edit_div_span);
-        edit_div_span.classList.toggle("To-do-add-div-show");
-        for(let i=0 ; i<todo_list_items.length ; i++){
-            if(todo_list_items[i]===todoList_span_div){
-                todo_list_items[i].remove();
-                todo_list_items.splice(i,1);
-                break;
-            }
-        }
-    }
+    // edit_image.addEventListener("click",edit_function);
+    // function edit_function(){
+    //     let edit_div_span = document.getElementsByClassName("To-do-add-div")[0];
+    //     todo_submit.innerHTML = "ویرایش تسک";
+    //     input_title.value = todoList_span_div_right_def_top_h4.innerHTML;
+    //     input_def.value = todoList_span_div_right_def_bottom.innerHTML;
+    //     edit_div_span.querySelector(".new_priority_tag p").innerHTML = priority_value.innerHTML;
+    //     edit_div_span.querySelector(".new_priority_tag").style.backgroundColor = todoList_span_div_right_line.style.backgroundColor;
+    //     edit_div_span.querySelector(".new_priority_tag").style.color = todoList_span_div_right_def_top_priority.style.color;      
+    //     todoList_span_div.insertAdjacentElement('afterend', edit_div_span);
+    //     edit_div_span.classList.toggle("To-do-add-div-show");
+    //     todo_submit.classList.add("down-bagcolor");
+    // }
 
 
+
+
+    eventlisteners(todoList_span_div);
 
 
 }
